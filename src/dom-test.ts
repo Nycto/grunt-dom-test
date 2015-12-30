c2988a3e0cbb0d667cf38c0f34468dd86eb47a6a
Nycto <James@RoundEights.com>
2015-12-30 11:10:59 -0800

    Add some types

--- src/dom-test.ts
+++ src/dom-test.ts
@@ -1,9 +1,15 @@
+/// <reference path="../typings/gruntjs/gruntjs.d.ts" />
+
+/**
+ * The list of valid options that can be passed to this module
+ */
+interface UserConfig {
+}
+
 /**
  * Primary entry point for the grunt task
  */
-declare var module: any;
-
-module.exports = function domTest( grunt, options ) {
+module.exports = function domTest( grunt: IGrunt, options: UserConfig ) {
     // noop
 };
 
